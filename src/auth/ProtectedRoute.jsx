c49e97cc8b538331d513faa9630bd55ed201de35
vipink28 <vipinkalyan28@gmail.com>
2023-03-28 10:47:49 +0530

    Protected route checked user from database also.

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function ProtectedRoute({children}) {
-    const navigate = useNavigate();
-    const [isLoggedIn, setIsLoggedIn]= useState();
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState();
 
-    const checkUserState = ()=>{
-        const user = localStorage.getItem("user");
-        if (user === "undefined" || !user) {
-          const userObj = JSON.parse(user);
-          setIsLoggedIn(false);
-          return navigate("/");
-        }
-        setIsLoggedIn(true);
-    }
+  const checkUserState = async () => {
+    const user = localStorage.getItem("user");
+    const userObj = JSON.parse(user);
 
-    useEffect(() => {
-        checkUserState();
-    }, [isLoggedIn]);
-
-    return (        
-        <>
-          {
-                isLoggedIn ? children : null
-            }
-        </>
+    const response = await fetch(
+      `http://localhost:5000/user?email=${userObj?.email}`,
+      { method: "GET" }
     );
+    const userData = await response.json();
+
+    if (user === "undefined" || !user || userData.length === 0) {
+      localStorage.removeItem("user");
+      setIsLoggedIn(false);
+      return navigate("/");
+    }
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    checkUserState();
+  }, [isLoggedIn]);
+
+  return <>{isLoggedIn ? children : null}</>;
 }
 
 export default ProtectedRoute;
